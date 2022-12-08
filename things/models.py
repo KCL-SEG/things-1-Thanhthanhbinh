fb69f230c2cc1339d12ad536a7ba3603e326ceb6
Binh Do <binh.do@kcl.ac.uk>
2022-12-08 14:47:18 +0000

    Add blank true to description

--- things/models.py
+++ things/models.py
@@ -4,6 +4,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=30,blank=False, unique=True)
-    description = models.CharField(max_length=120)
+    description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(default=0, validators=[MinValueValidator(0),
                                                         MaxValueValidator(100)])
