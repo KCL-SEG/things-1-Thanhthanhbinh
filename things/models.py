4717d7f82ee0b6fdaad059584e1a4e2d3e5d3c3b
Binh Do <binh.do@kcl.ac.uk>
2022-12-08 14:09:28 +0000

    Fix template

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Things(models.Model):
+    name = models.CharField(max_length=5)
+    description = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
